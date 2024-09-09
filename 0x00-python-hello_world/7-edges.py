206827f3b2c4323492e48551545d89e0ee34fd42
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-09 20:59:32 +0200

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Task 7: Manipulate a string by slicing and printing different parts
 word = "Holberton"
 word_first_3 = word[:3]  # First 3 letters of the string
 word_last_2 = word[-2:]  # Last 2 letters of the string
