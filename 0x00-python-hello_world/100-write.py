a83489f14c473f5cb4119b1d3e4a27843be032c5
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-09 18:52:57 +0200

    Create 100-write.py

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Task 11: Print a specific message to stderr and exit with status code 1
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
