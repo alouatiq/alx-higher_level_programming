a28d1c6cafbefa2f020cc65cdd5d83bce86e28f6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-09 18:53:18 +0200

    Create 102-magic_calculation.py

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Task 13: Reproduce specific bytecode behavior by implementing a function
+def magic_calculation(a, b):
+    return 98 + (a ** b)  # Raise a to the power of b, then add 98
