ba5475b141bb41c8c66b536bcf7ddbecc241667d
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-09 18:57:45 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,26 @@
 #include "lists.h"
 
-/*
- * Task 10: Function to detect if there is a cycle in a singly linked list
- * Uses Floyd's Cycle Detection Algorithm (Tortoise and Hare method)
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the linked list
+ *
+ * Return: 1 if there is a cycle, 0 if there is no cycle
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list;
-    listint_t *fast = list;
+    listint_t *slow = list;  // Slow pointer, moves one step at a time
+    listint_t *fast = list;  // Fast pointer, moves two steps at a time
 
+    // Traverse the list as long as fast and fast->next are not NULL
     while (slow && fast && fast->next)
     {
-        slow = slow->next;
-        fast = fast->next->next;
+        slow = slow->next;         // Move slow pointer by one step
+        fast = fast->next->next;   // Move fast pointer by two steps
 
-        if (slow == fast)
-            return (1);  // Cycle detected
+        if (slow == fast)          // If both pointers meet, there is a cycle
+            return (1);
     }
-    return (0);  // No cycle
+
+    // If we reach here, no cycle was found
+    return (0);
 }
