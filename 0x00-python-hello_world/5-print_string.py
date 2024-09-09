6a52e6cfa67adf666a73f54b87c08779203f04ea
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-09 20:58:42 +0200

    Update 5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Task 5: Print the string 3 times, followed by the first 9 characters of the string
 str = "Holberton School"
 print(str * 3)  # Print the string 3 times
 print(str[:9])  # Print the first 9 characters of the string
