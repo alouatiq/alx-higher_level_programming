4773871e943041732070e4267924a6373952a116
AL OUATIQ H <alouatiq@outlook.com>
2024-09-09 11:16:17 -0700

    add description to lists.h

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,19 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list;  // Slow pointer, moves one step at a time
-    listint_t *fast = list;  // Fast pointer, moves two steps at a time
+	listint_t *slow = list;  // Slow pointer, moves one step at a time
+	listint_t *fast = list;  // Fast pointer, moves two steps at a time
 
-    // Traverse the list as long as fast and fast->next are not NULL
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;         // Move slow pointer by one step
-        fast = fast->next->next;   // Move fast pointer by two steps
+	// Traverse the list as long as fast and fast->next are not NULL
+	while (slow && fast && fast->next)
+	{
+	    slow = slow->next;         // Move slow pointer by one step
+	    fast = fast->next->next;   // Move fast pointer by two steps
 
-        if (slow == fast)          // If both pointers meet, there is a cycle
-            return (1);
-    }
+	    if (slow == fast)          // If both pointers meet, there is a cycle
+	        return (1);
+	}
 
-    // If we reach here, no cycle was found
-    return (0);
+	// If we reach here, no cycle was found
+	return (0);
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -6,11 +6,15 @@
 /* 
  * Task 10: Define a struct for a singly linked list
  * Prototype for the check_cycle function to detect cycles in a linked list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int check_cycle(listint_t *list);
