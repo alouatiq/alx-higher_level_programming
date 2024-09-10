971a06e9f52d79135bef990db8cec233dd588b12
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-10 19:21:00 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(str(i) + str(j))
+            print(f"{i}{j}")
         else:
-            print(str(i) + str(j) + ", ", end="")
+            print(f"{i}{j}, ", end="")
