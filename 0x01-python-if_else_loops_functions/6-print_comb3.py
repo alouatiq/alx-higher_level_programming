92dada527834c97d7b43d62d7cb69544ea661264
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-10 19:26:35 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for first_digit in range(9):
-    for second_digit in range(x + 1, 10):
-        if first_digit == 8:
+for x in range(9):
+    for y in range(x + 1, 10):
+        if x == 8:
             # For the last combination (89), print without a comma and space
             print(f"{x}{y}")
             break
