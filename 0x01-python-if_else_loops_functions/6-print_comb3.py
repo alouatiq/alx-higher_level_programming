e798c6bd87b740fc48a2232fcba2e0d07a842996
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-10 22:23:42 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for first_digit in range(9):
-    for second_digit in range(first_digit + 1, 10):
-        if first_digit == 8:
-            print(f"{first_digit}{second_digit}")
-            break
-        print(f"{first_digit}{second_digit}", end=", ")
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
