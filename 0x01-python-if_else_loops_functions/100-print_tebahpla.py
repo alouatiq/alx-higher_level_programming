a08b478c5e4a6cd989ac975d32e1397c055d13a7
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-10 20:42:34 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print(f"{chr(i - 32) if i % 2 else chr(i)}", end="")
+print("".join(["%c" % (i - 32 if i % 2 else i) for i in range(122, 96, -1)]), end="")
