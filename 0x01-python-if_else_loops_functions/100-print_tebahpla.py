e2aefd50300de333f2ba7aa97470127220af2fa0
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-10 22:37:51 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 for i in range(122, 96, -1):  # ASCII values from 'z' (122) to 'a' (97)
     if i % 2 == 0:  # Even ASCII values should be lowercase
         print("{}".format(chr(i)), end="")
-    else:  # Odd ASCII values should be uppercase
+    else:  ## Odd ASCII values should be uppercase
         print("{}".format(chr(i - 32)), end="")
