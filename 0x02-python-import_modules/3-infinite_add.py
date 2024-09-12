9c8932051ccaf8e11358b6b6f2f1e8743111679e
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 16:33:33 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys  # Import sys to access command-line arguments
 
-result = sum(int(arg) for arg in sys.argv[1:])
-print(result)
+    # Initialize the total sum to 0
+    total_sum = 0
+
+    # Iterate over all command-line arguments starting from index 1 (ignoring the script name)
+    for arg in sys.argv[1:]:
+        total_sum += int(arg)  # Convert each argument to an integer and add to the total sum
+
+    # Print the total sum
+    print(total_sum)
