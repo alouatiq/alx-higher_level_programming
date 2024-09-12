7a2c8e2543b0b6c56f333df15139ccb0ec6a6a80
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 15:24:02 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add  # Importing the add function
+    from add_0 import add  # Importing add function once
 
-    a = 1  # First variable
-    b = 2  # Second variable
+    a = 1  # Assign 1 to a
+    b = 2  # Assign 2 to b
 
-    # Print the result using formatted string
-    print(f"{a} + {b} = {add(a, b)}")
+    # Print the result of add(a, b) using formatted string
+    print("{} + {} = {}".format(a, b, add(a, b)))
