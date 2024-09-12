feeef0aec86108dc9e3d3a6bf69dea4b92682c91
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 16:16:01 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys  # Import sys to access command-line arguments
+    import sys
 
-    # Initialize total sum to 0
     total_sum = 0
-
-    # Iterate over command-line arguments starting from index 1 (skipping the script name)
+    
     for arg in sys.argv[1:]:
-        total_sum += int(arg)  # Convert each argument to an integer and add to total_sum
-
-    # Print the total sum followed by a new line
+        total_sum += int(arg)
     print(total_sum)
