6e5f775bec54d77166eb86f729026b8924f721bb
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 16:20:17 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__":
+    import hidden_4  # Import the hidden_4 module
 
-names = dir(hidden_4)
-for name in sorted(names):
-    if not name.startswith("__"):
+    # Get all names defined by the module using dir()
+    names = dir(hidden_4)
+
+    # Filter out names starting with '__'
+    filtered_names = [name for name in names if not name.startswith("__")]
+
+    # Sort the names in alphabetical order and print each one
+    for name in sorted(filtered_names):
         print(name)
