4cb88245aaf7d4a2664c0493149198aac824652a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 15:55:10 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    # Importing the functions from calculator_1.py
+    # Import only once from calculator_1
     from calculator_1 import add, sub, mul, div
 
     # Assign values to a and b on two separate lines
     a = 10
     b = 5
 
-    # Print results for each operation using 4 print statements
+    # Perform calculations and print results (only 4 print statements)
     print("{} + {} = {}".format(a, b, add(a, b)))  # Addition
     print("{} - {} = {}".format(a, b, sub(a, b)))  # Subtraction
     print("{} * {} = {}".format(a, b, mul(a, b)))  # Multiplication
