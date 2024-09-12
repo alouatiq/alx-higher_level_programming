a62b2313d0ac6ff01e14e9f5bdd5a348bf0126dc
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 15:50:06 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,18 +3,12 @@ if __name__ == "__main__":
     # Importing functions from calculator_1.py
     from calculator_1 import add, sub, mul, div
 
-    # Assigning values to a and b on two different lines
+    # Assign values to a and b on separate lines
     a = 10
     b = 5
 
-    # Perform calculations and print results
-    result_add = add(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = int(div(a, b))  # Ensure result is an integer
-
-    # Printing results (with exactly 4 print statements)
-    print("{} + {} = {}".format(a, b, result_add))
-    print("{} - {} = {}".format(a, b, result_sub))
-    print("{} * {} = {}".format(a, b, result_mul))
-    print("{} / {} = {}".format(a, b, result_div))  # Ensure division result is an integer
+    # Use exactly 4 print statements to display the results
+    print("{} + {} = {}".format(a, b, add(a, b)))  # Addition
+    print("{} - {} = {}".format(a, b, sub(a, b)))  # Subtraction
+    print("{} * {} = {}".format(a, b, mul(a, b)))  # Multiplication
+    print("{} / {} = {}".format(a, b, div(a, b)))  # Division
