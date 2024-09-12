5a70c358dd9741df10631e9e5071d48d4606a586
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-12 15:46:17 +0200

    Update calculator_1.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    """Addition function"""
     return a + b
 
 def sub(a, b):
-    """Subtraction function"""
     return a - b
 
 def mul(a, b):
-    """Multiplication function"""
     return a * b
 
 def div(a, b):
-    """Division function"""
-    return int(a / b)  # Ensure division returns an integer
+    return int(a / b)  # Division that returns an integer
