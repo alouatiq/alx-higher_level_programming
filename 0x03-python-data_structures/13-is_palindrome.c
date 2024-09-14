b077c24fb9da11f9fdfa1de16cb2d64e02bde4a1
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-14 18:14:32 +0200

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,26 +1,32 @@
 #include "lists.h"
 
-void reverse(listint_t **head);
-int compare_lists(listint_t *head1, listint_t *head2);
-
-int is_palindrome(listint_t **head) {
-	if (*head == NULL || (*head)->next == NULL) {
-	    return 1;
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the head of the list
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (1);
 	}
 
 	listint_t *slow = *head, *fast = *head;
 	listint_t *prev_slow = NULL, *mid_node = NULL;
 	listint_t *second_half;
-	
-	while (fast != NULL && fast->next != NULL) {
-	    fast = fast->next->next;
-	    prev_slow = slow;
-	    slow = slow->next;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev_slow = slow;
+		slow = slow->next;
 	}
 
-	if (fast != NULL) {
-	    mid_node = slow;
-	    slow = slow->next;
+	if (fast != NULL)
+	{
+		mid_node = slow;
+		slow = slow->next;
 	}
 
 	second_half = slow;
@@ -30,34 +36,55 @@ int is_palindrome(listint_t **head) {
 	int result = compare_lists(*head, second_half);
 
 	reverse(&second_half);
-	if (mid_node != NULL) {
-	    prev_slow->next = mid_node;
-	    mid_node->next = second_half;
-	} else {
-	    prev_slow->next = second_half;
+
+	if (mid_node != NULL)
+	{
+		prev_slow->next = mid_node;
+		mid_node->next = second_half;
+	}
+	else
+	{
+		prev_slow->next = second_half;
 	}
 
-	return result;
+	return (result);
 }
 
-void reverse(listint_t **head) {
+/**
+ * reverse - reverses a singly linked list
+ * @head: double pointer to the head of the list
+ */
+void reverse(listint_t **head)
+{
 	listint_t *prev = NULL, *current = *head, *next;
-	while (current != NULL) {
-	    next = current->next;
-	    current->next = prev;
-	    prev = current;
-	    current = next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
+
 	*head = prev;
 }
 
-int compare_lists(listint_t *head1, listint_t *head2) {
-	while (head1 != NULL && head2 != NULL) {
-	    if (head1->n != head2->n) {
-	        return 0;
-	    }
-	    head1 = head1->next;
-	    head2 = head2->next;
+/**
+ * compare_lists - compares two linked lists
+ * @head1: first linked list
+ * @head2: second linked list
+ * Return: 1 if identical, 0 otherwise
+ */
+int compare_lists(listint_t *head1, listint_t *head2)
+{
+	while (head1 != NULL && head2 != NULL)
+	{
+		if (head1->n != head2->n)
+		{
+			return (0);
+		}
+		head1 = head1->next;
+		head2 = head2->next;
 	}
 	return (head1 == NULL && head2 == NULL);
 }
