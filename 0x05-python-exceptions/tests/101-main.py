a1e476e7042e7507b677d33d9d1fe475d83204a6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:47:59 +0200

    Create 101-main.py

---
+++ 0x05-python-exceptions/tests/101-main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+safe_function = __import__('101-safe_function').safe_function
+
+# Test with a valid function
+def my_div(a, b):
+    return a / b
+
+result = safe_function(my_div, 10, 2)
+print("result of my_div: {}".format(result))
+
+# Test with division by zero
+result = safe_function(my_div, 10, 0)
+print("result of my_div: {}".format(result))
+
+# Test with another function that raises an index error
+def print_list(my_list, length):
+    for i in range(length):
+        print(my_list[i])
+    return len(my_list)
+
+result = safe_function(print_list, [1, 2, 3, 4], 10)
+print("result of print_list: {}".format(result))
