c55f32424cdb921050b7f9da5483f7e1552d4180
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:43:47 +0200

    Create 103-python.c

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,13 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p) {
+    // Implementation for printing Python List info
+}
+
+void print_python_bytes(PyObject *p) {
+    // Implementation for printing Python Bytes info
+}
+
+void print_python_float(PyObject *p) {
+    // Implementation for printing Python Float info
+}
