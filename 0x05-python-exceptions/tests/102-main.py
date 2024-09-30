0c8511e62082d9bb5d0664c8961a63f1d45645b8
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:48:09 +0200

    Create 102-main.py

---
+++ 0x05-python-exceptions/tests/102-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+# Test magic_calculation
+print(magic_calculation(2, 3))
+print(magic_calculation(10, 4))
