2b206daf77614813d69e7bf94450ef6d1c976a55
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:47:31 +0200

    Create 5-main.py

---
+++ 0x05-python-exceptions/tests/5-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+raise_exception = __import__('5-raise_exception').raise_exception
+
+# Test to check if TypeError is raised
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
