9bb885555a76104846493ab1b149c4474382a716
AL OUATIQ <alouatiq@outlook.com>
2024-09-30 10:59:51 -0700

    testing

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,13 +1,16 @@
 #include <Python.h>
 
-void print_python_list(PyObject *p) {
-    // Implementation for printing Python List info
+void print_python_list(PyObject *p)
+{
+  /* Implementation for printing Python List info */
 }
 
-void print_python_bytes(PyObject *p) {
-    // Implementation for printing Python Bytes info
+void print_python_bytes(PyObject *p)
+{
+  /* Implementation for printing Python Bytes info */
 }
 
-void print_python_float(PyObject *p) {
-    // Implementation for printing Python Float info
+void print_python_float(PyObject *p)
+{
+  /* Implementation for printing Python Float info */
 }
