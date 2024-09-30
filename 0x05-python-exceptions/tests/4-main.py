8216729ec9145634bade652a1ec67492eb51a0c3
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:47:22 +0200

    Create 4-main.py

---
+++ 0x05-python-exceptions/tests/4-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+list_division = __import__('4-list_division').list_division
+
+# Test with valid lists
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, 3)
+print(result)
+
+print("--")
+
+# Test with various issues (zero division, wrong type, out of range)
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, 5)
+print(result)
