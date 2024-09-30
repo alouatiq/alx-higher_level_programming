3b71a496aa85c67455d26dbf5c988d56fe8feba6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:47:07 +0200

    Create 3-main.py

---
+++ 0x05-python-exceptions/tests/3-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+safe_print_division = __import__('3-safe_print_division').safe_print_division
+
+# Test with valid division
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+# Test division by zero
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
