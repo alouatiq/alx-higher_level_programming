bc04d1979a1b5cad1bfee80475d3117534941e01
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-30 19:46:58 +0200

    Create 2-main.py

---
+++ 0x05-python-exceptions/tests/2-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_list_integers
+
+# List of integers
+my_list = [1, 2, 3, 4, 5]
+
+# Test with the first 2 integers
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+# Mixed list of integers and non-integers
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+# Test with more elements than available
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
