584323636e4e5b47d4ae69d3e0422e7e7fec2ff2
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-01 20:20:14 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,10 @@
 This module defines a class called Square with a private attribute.
 """
 
+
 class Square:
     """A class that defines a square by its size."""
-    
+
     def __init__(self, size):
         """
         Initializes the square with a private size attribute.
