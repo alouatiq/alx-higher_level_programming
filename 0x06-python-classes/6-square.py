e8396b041380026bde23c60dc8401b7b2c0bc1c0
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-01 20:23:26 +0200

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,8 +3,12 @@
 This module defines a class Square with size, position, and printing.
 """
 
+
 class Square:
-    """A class that defines a square with size, position, area, and printing."""
+    """
+    A class that defines a square with size,
+    position, area, and printing.
+    """
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the square with size and position validation."""
@@ -45,7 +49,10 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Prints the square with the character # or an empty line if size is 0."""
+        """
+        Prints the square with the character #
+        or an empty line if size is 0.
+        """
         if self.__size == 0:
             print("")
         else:
