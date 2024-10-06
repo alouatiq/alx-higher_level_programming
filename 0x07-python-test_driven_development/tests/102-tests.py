55230948bc15de19e02f71b172ea0b5f2074722e
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-06 09:27:11 +0200

    Update 102-tests.py

--- 0x07-python-test_driven_development/tests/102-tests.py
+++ 0x07-python-test_driven_development/tests/102-tests.py
@@ -4,22 +4,22 @@ lib = ctypes.CDLL('./libPython.so')
 lib.print_python_string.argtypes = [ctypes.py_object]
 
 s = "The spoon does not exist"
-lib.print_python_string(s)
+lib.print_python_string(s)  # Expected output: string info
 
 s = "ложка не существует"
-lib.print_python_string(s)
+lib.print_python_string(s)  # Expected output: string info
 
 s = "La cuillère n'existe pas"
-lib.print_python_string(s)
+lib.print_python_string(s)  # Expected output: string info
 
 s = "勺子不存在"
-lib.print_python_string(s)
+lib.print_python_string(s)  # Expected output: string info
 
 s = "숟가락은 존재하지 않는다."
-lib.print_python_string(s)
+lib.print_python_string(s)  # Expected output: string info
 
 s = "スプーンは存在しない"
-lib.print_python_string(s)
+lib.print_python_string(s)  # Expected output: string info
 
-s = b"The spoon does not exist"  # Not a valid Python string
+s = b"The spoon does not exist"  # Expected error: invalid string
 lib.print_python_string(s)
