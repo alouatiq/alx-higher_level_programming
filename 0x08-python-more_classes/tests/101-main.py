abc639c92b83434d0003799cfe4a30f0e0a87dfc
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-07 08:40:49 +0200

    Create 101-main.py

---
+++ 0x08-python-more_classes/tests/101-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+from 101-nqueens import nqueens
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(N)
