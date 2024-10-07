ffdb402fe4551dd922aec52fbed626ab0fecbbef
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-07 20:20:31 +0200

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -84,7 +84,8 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Return a string representation of the rectangle using the print_symbol.
+        """Return a string representation of the rectangle
+        using the print_symbol.
 
         Returns:
             str: A string representation of the rectangle, or an empty
@@ -104,7 +105,8 @@ class Rectangle:
         return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
-        """Print a message when a Rectangle instance is deleted and decrement the instance count."""
+        """Print a message when a Rectangle instance is
+        deleted and decrement the instance count."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
@@ -118,7 +120,8 @@ class Rectangle:
         Raises:
             TypeError: If rect_1 or rect_2 is not an instance of Rectangle.
         Returns:
-            Rectangle: The rectangle with the larger area, or rect_1 if both are equal.
+            Rectangle: The rectangle with the larger area,
+            or rect_1 if both are equal.
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
