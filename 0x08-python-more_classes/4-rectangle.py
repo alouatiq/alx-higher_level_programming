932451763a3820c43162884e34e0bd53abe6aa78
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-07 08:35:29 +0200

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle with width, height, area, perimeter, string representation, and eval."""
+"""Defines a class Rectangle with width, height, area,
+perimeter, string representation, and eval."""
 
 
 class Rectangle:
