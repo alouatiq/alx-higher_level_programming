2937725ed3c02de12ed8c431c1dc7f7316c873f5
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-14 12:50:41 +0200

    Create 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """MyInt class is a rebel, it inverts == and != operators."""
+    
+    def __eq__(self, other):
+        """Inverts the == operator."""
+        return super().__ne__(other)
+    
+    def __ne__(self, other):
+        """Inverts the != operator."""
+        return super().__eq__(other)
