cadb07ff6aa6793ff54c1182bacd8f936856dbd1
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-14 21:43:17 +0200

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""T101"""
+
+
 def add_attribute(obj, name, value):
-    """Adds a new attribute to an object if possible, else raises a TypeError."""
+    """Adds a new attribute to an object
+    if possible, else raises a TypeError."""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
