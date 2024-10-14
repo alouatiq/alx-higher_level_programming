f6450998674ea70fbcc95091322a8c68e07a2ddd
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-14 21:41:45 +0200

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""T9"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry with area and str methods."""
+    """Rectangle class that inherits from
+    BaseGeometry with area and str methods."""
 
     def __init__(self, width, height):
-        """Initializes width and height, validating them as positive integers."""
+        """Initializes width and height
+        validating them as positive integers."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
