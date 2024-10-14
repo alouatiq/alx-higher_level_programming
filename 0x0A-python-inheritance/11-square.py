1cb54c592179818fd206f8fc9d80b1d299aef503
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-14 12:50:34 +0200

    Create 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle with a custom str method."""
+    
+    def __init__(self, size):
+        """Initializes a square with the given size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+    
+    def __str__(self):
+        """Returns a formatted string description of the square."""
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
