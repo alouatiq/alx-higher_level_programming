0f9d11e0428cf2197dd0425007d65f5af104c101
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-14 21:32:41 +0200

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,10 @@
 """Module for task 1"""
 
 class MyList(list):
-    """Inherits from list with additional print_sorted method"""
+    """Inherits from list with additional
+    print_sorted method"""
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order"""
+        """Prints the list in ascending
+        sorted order"""
         print(sorted(self))
