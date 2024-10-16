8e6ea1784853e51c35810c7be8c0e7cc8c326967
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:16:10 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Module that contains a function to write text to a file."""
+"""Module that contains a function
+to write text to a file."""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """Writes a string to a text file (UTF8) and
+    returns the number of characters written."""
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
