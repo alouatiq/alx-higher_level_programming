f2646c8645d87972643f81bf3f597454e53b0b7e
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:06:55 +0200

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contains a function to read a file and print its contents to stdout."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
