cb2cb5caad5fa0c1f278bd90f313fb9104190d71
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:18:55 +0200

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,14 +3,17 @@
 
 
 class Student:
-    """Represents a student with first name, last name, and age."""
+    """Represents a student with first
+    name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the student with the provided attributes."""
+        """Initializes the student with
+        the provided attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of a Student instance."""
+        """Retrieves a dictionary representation
+        of a Student instance."""
         return self.__dict__
