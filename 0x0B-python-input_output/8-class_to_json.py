60b6074c1ed2ffb982fc9ca78024d15348732513
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:07:59 +0200

    Create 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that contains a function to convert a class instance to JSON-compatible dictionary."""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description for JSON serialization of an object."""
+    return obj.__dict__
