3e45d72587b0648546a0800edb3023fc109e8d5e
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:17:20 +0200

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""Module that defines a Student class with serialization and deserialization."""
+"""Module that defines a Student class
+with serialization and deserialization."""
 
 
 class Student:
-    """Represents a student with first name, last name, and age."""
+    """Represents a student with first
+    name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the student with the provided attributes."""
+        """Initializes the student with
+        the provided attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only those attributes are retrieved.
+        """Retrieves a dictionary
+        representation of a Student instance.
+        If attrs is a list of strings
+        only those attributes are retrieved.
         """
         if isinstance(attrs, list) and all(isinstance(attr, str)
                                            for attr in attrs):
@@ -23,6 +28,7 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance."""
+        """Replaces all attributes of
+        the Student instance."""
         for key, value in json.items():
             setattr(self, key, value)
