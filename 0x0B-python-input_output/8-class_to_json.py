118dceeef705e292b78940c48649066c65912fa7
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:15:39 +0200

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,5 +4,6 @@ class instance to JSON-compatible dictionary."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object."""
+    """Returns the dictionary description for
+    JSON serialization of an object."""
     return obj.__dict__
