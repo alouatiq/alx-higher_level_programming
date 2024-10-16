aa1366f5f22062194255d16ac144e29fb7621ff7
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-16 17:18:07 +0200

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Module that contains a function to convert a JSON string to a Python object."""
+"""Module that contains a function to
+convert a JSON string to a Python object."""
 import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string."""
+    """Returns an object (Python data structure)
+    represented by a JSON string."""
     return json.loads(my_str)
