8f93154bd61e12493c22f77024a9e41871208e8a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-17 10:04:10 +0200

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module containing the Square class"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Override __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Update attributes"""
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for i, value in enumerate(args):
+                setattr(self, attrs[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
