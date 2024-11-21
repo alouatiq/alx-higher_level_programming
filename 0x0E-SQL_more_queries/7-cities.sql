961fcdb82c043d878ebd37cad2f27078807ad828
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:27:29 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,10 @@
+-- Script to create the database 'hbtn_0d_usa' and the table 'cities'
+-- The table 'cities' stores city information with a FOREIGN KEY to 'states'
 
--- Create table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
