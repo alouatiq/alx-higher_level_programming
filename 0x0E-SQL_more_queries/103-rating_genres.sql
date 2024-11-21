d18ddfcc792d7dfcb5b2e0e4e234388c7b34604a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:50:28 +0100

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,9 +2,16 @@
 -- Each record displays the genre name and the total rating
 -- Results are sorted in descending order by the total rating
 
-SELECT tv_genres.name AS genre_name, SUM(tv_show_ratings.rate) AS total_rating
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
-ORDER BY total_rating DESC;
+SELECT 
+    tv_genres.name AS name,
+    SUM(tv_show_ratings.rate) AS rating
+FROM 
+    tv_genres
+LEFT JOIN 
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN 
+    tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY 
+    tv_genres.name
+ORDER BY 
+    rating DESC;
