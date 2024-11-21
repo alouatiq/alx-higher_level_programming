304d4234b117a3e97d9985ad485949ced2f97e0a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:55:08 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,11 @@
--- Convert the database hbtn_0c_0 to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Use the database hbtn_0c_0
+-- Switch to the database
 USE hbtn_0c_0;
 
--- Convert the table first_table to UTF8
+-- Alter the database to use UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Alter the table to use UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the name column in first_table to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Alter the specific column to use UTF8
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
