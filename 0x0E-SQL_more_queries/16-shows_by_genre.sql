c61c2bbc8b9d0cf201327bea1b951e6fe92b6bfd
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:46:11 +0100

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
--- Script to list all shows and their linked genres
--- Each record displays the show title and genre name (NULL if no genre is linked)
+-- Script to list all TV shows and their associated genres
+-- Each record displays the show title and the genre name
 -- Results are sorted in ascending order by the show title and genre name
 
-SELECT tv_shows.title, tv_genres.name AS genre
+SELECT tv_shows.title AS show_title, tv_genres.name AS genre_name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
