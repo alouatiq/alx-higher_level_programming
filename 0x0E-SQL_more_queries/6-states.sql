83839565c01678d28f96c2b88b8f78ad8fddb6a8
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:26:03 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,10 @@
+-- Script to create the database 'hbtn_0d_usa' and the table 'states'
+-- The database will store state information with 'id' as PRIMARY KEY and 'name' as NOT NULL
 
--- Create table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
