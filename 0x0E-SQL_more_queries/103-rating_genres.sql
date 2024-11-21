f413afcab52e48467d6ffc1550b5ffc08484d858
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:47:09 +0100

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,8 @@
--- Script to list all genres by their cumulative ratings
--- Each record displays the genre name and the total rating sum
+-- Script to list all genres by their rating sum
+-- Each record displays the genre name and the total rating
 -- Results are sorted in descending order by the total rating
 
-SELECT tv_genres.name AS genre, SUM(tv_show_ratings.rate) AS total_rating
+SELECT tv_genres.name AS genre_name, SUM(tv_show_ratings.rate) AS total_rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
