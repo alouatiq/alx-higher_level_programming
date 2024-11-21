2c91cba89b83728e13c1492d6407fbdb981d17cf
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:42:50 +0100

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,13 @@
+-- Script to list all shows that are not linked to the "Comedy" genre
+-- Each record displays the show title
+-- Results are sorted in ascending order by the show title
 
--- List all shows without Comedy
-SELECT title 
-FROM tv_shows 
-WHERE id NOT IN (
-    SELECT tv_show_id 
-    FROM tv_show_genres 
-    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+SELECT DISTINCT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy'
 )
-ORDER BY title ASC;
+ORDER BY tv_shows.title ASC;
