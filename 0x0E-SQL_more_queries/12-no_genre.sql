6a0ef06b1c4209261bd7d1b06257709d21f31615
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-21 11:37:31 +0100

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,9 @@
+-- Script to list all shows without any genre linked
+-- Each record displays the show title and genre ID as NULL
+-- Results are sorted in ascending order by show title
 
--- List shows without genres
-SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_shows 
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id 
-WHERE tv_show_genres.genre_id IS NULL 
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC;
