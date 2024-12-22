5244bb14dda79a8ba1e14c691452037a29f03c7a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-22 20:08:22 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Displays all values in the states table where name matches the argument
+(safe from SQL injection)
 """
 import MySQLdb
 import sys
@@ -9,9 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    query = ("SELECT * FROM states WHERE name LIKE BINARY '{}' "
-         "ORDER BY id ASC".format(sys.argv[4]))
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
+                (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
