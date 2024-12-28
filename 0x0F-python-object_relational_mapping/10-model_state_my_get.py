5c5d335b59ccaefb2fe7cf3e48e2dd357744c1b8
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-28 11:59:00 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as an argument from the database hbtn_0e_6_usa
+Prints the State object with the name passed as an
+argument from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
@@ -20,4 +21,4 @@ if __name__ == "__main__":
         print("Not found")
 
     session.close()
-
+    
