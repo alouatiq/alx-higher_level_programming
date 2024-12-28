1689540b07c215f9ac405df125ca2c3ca5c899a4
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-28 12:03:20 +0100

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
         pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
@@ -20,4 +21,3 @@ if __name__ == "__main__":
     print(new_state.id)
 
     session.close()
-
