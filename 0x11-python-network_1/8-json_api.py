a3a985d7a57dc586454cd82d1dde9e095ceab2a7
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-17 12:21:45 +0100

    Create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Sends a POST request with a letter parameter to
+an API and processes JSON response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    response = requests.post(url, data={"q": q})
+    try:
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
