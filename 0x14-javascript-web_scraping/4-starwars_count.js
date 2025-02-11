5402b7fbd18fc93800b41be924ab90ada40bd1de
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-11 13:18:10 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-request(apiUrl, { json: true }, (error, response, body) => {
+const url = process.argv[2]; // The API URL for films
+
+request(url, { json: true }, (error, response, body) => {
   if (error) {
-    console.error('error:', error);
-  } else if (response.statusCode === 200 && body.results) {
-    const films = body.results;
-    let count = 0;
-    films.forEach((film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`)) {
-        count++;
-      }
-    });
-    console.log(count);
-  } else {
-    console.log(`Failed to get a valid response: ${response.statusCode}`);
+    console.error('Error:', error);
+    return;
   }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to get a valid response: ${response.statusCode}`);
+    return;
+  }
+  const films = body.results;
+  let count = 0;
+  // Wedge Antilles has ID 18, so his URL is:
+  const wedgeURL = 'https://swapi-api.alx-tools.com/api/people/18/';
+  films.forEach(film => {
+    if (film.characters.includes(wedgeURL)) {
+      count++;
+    }
+  });
+  console.log(count);
 });
