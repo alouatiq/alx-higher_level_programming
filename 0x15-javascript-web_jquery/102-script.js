1b5b410a31d5d1a94b6dd5949bafe71776ba903b
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-19 09:57:18 +0100

    Create 102-script.js

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#btn_translate").click(function () {
+        const langCode = $("#language_code").val();
+        if (langCode) {
+            $.get(`https://www.fourtonfish.com/hellosalut/?lang=${langCode}`, function (data) {
+                $("#hello").text(data.hello);
+            });
+        }
+    });
+});
